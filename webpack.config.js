0f32b65f39d8dc6c5d9379cbd3ca80b23826019b
michal-jankowski <michaljankowski86@gmail.com>
2018-03-08 17:42:41 +0100

    Code Splitting - 2 and environment settings

--- webpack.config.js
+++ webpack.config.js
@@ -4,87 +4,93 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Obiekt konfiguracyjny webpacka
-module.exports = {
+module.exports = function(env) {
 
-    // entry point
-    entry: {
-        app: "./src/js/app.js"
-    },
+    var prod = env !== undefined && env.production === true;
+    var dev = env !== undefined && env.development === true;
+    
+    return {
 
-    // output object
-    output: {
-        publicPath: "/dist/",
-        // dla właściwości path musimy podać ścieżkę absolutną (patrz import modułu path)
-        path: resolve(__dirname, "dist/"),
-        filename: "[name].js"
-    },
+        // entry point
+        entry: {
+            app: "./src/js/app.js"
+        },
 
-    // Code Splitting, to read more: https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
-                }
-            }
-        }
-    },
+        // output object
+        output: {
+            publicPath: dev ? "/dist/" : "",
+            // dla właściwości path musimy podać ścieżkę absolutną (patrz import modułu path)
+            path: resolve(__dirname, "dist/"),
+            filename: prod ? "[name].[chunkhash].js" : "[name].js"
+        },
 
-    // Modules
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader!sass-loader"
-                })
-            },
-
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 10000,
-                            name: "[name].[ext]"
-                        }
+        // Code Splitting, to read more: https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendors",
+                        chunks: "all"
                     }
-                ]
-            },
-
-            // HANDLEBARS LOADER (optional)
-            {
-                test: /\.hbs$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "handlebars-template-loader"
                 }
             }
-        ]
-    },
+        },
 
-    plugins: [
-        new ExtractTextPlugin("main.css"),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
+        // Modules
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
 
-    node: {
-        fs: "empty" // avoids error messages (check documentation: https://github.com/emaphp/handlebars-template-loader)
-    }
+                {
+                    test: /\.scss$/,
+                    exclude: /node_modules/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: "css-loader!sass-loader"
+                    })
+                },
 
-};
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 10000,
+                                name: "[name].[ext]"
+                            }
+                        }
+                    ]
+                },
+
+                // HANDLEBARS LOADER (optional)
+                {
+                    test: /\.hbs$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "handlebars-template-loader"
+                    }
+                }
+            ]
+        },
+
+        plugins: [
+            new ExtractTextPlugin("main.css"),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            })
+        ],
+
+        node: {
+            fs: "empty" // avoids error messages (check documentation: https://github.com/emaphp/handlebars-template-loader)
+        }
+
+    };
+}
